4faeeb6e641ed3713e90441618036cae236e1799
Tushar0508 <93516921+Tushar0508@users.noreply.github.com>
2025-01-13 12:50:24 +0530

    Time: 109 ms (27.02%) | Memory: 32.6 MB (21.37%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     int minimumLength(string s) {
-        unordered_map<char, int> freq; 
-        int res = 0;
-
-        for (char ch : s) {
-            freq[ch]++;
+        map<char,int>m;
+        for(char ch: s){
+            m[ch]++;
         }
-
-        for (auto& [k, v] : freq) {
-            if (v > 2) {
-                if (v % 2 == 0) {
-                    res += 2;
-                } else {
-                    res += 1;
-                }
-            } else {
-                res += v;
-            }
+        int ans =0;
+        for(auto i:m){
+            if(i.second<3) ans+=i.second;
+            else if(i.second%2) ans++;
+            else ans+=2;
         }
-
-        return res;
+        return ans;
     }
 };
